985ca0b0a9611416bb69e3003663fc34299592a8
darksv <darek969-12@o2.pl>
2024-03-02 08:53:44 +0100

    feature: start working on module parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-    println!("Hello, world!");
+use crate::parser::{Item, ParserError, Reader, SectionKind, TypeKind};
+
+mod parser;
+
+#[derive(Debug)]
+struct FuncSignature {
+    params: Vec<TypeKind>,
+    results: Vec<TypeKind>,
+}
+
+impl Item for FuncSignature {
+    fn read(reader: &mut Reader, _offset: usize) -> Result<Self, ParserError> {
+        let num_params = reader.read_usize()?;
+        let mut params = Vec::new();
+        for _ in 0..num_params {
+            params.push(reader.read::<TypeKind>()?);
+        }
+
+        let num_results = reader.read_usize()?;
+        let mut results = Vec::new();
+        for _ in 0..num_results {
+            results.push(reader.read::<TypeKind>()?);
+        }
+
+        Ok(FuncSignature {
+            params,
+            results,
+        })
+    }
+}
+
+fn main() -> Result<(), ParserError> {
+    let mut reader = Reader::new(include_bytes!("../tests/simple.wasm"));
+
+    const WASM_MAGIC: &'static [u8; 4] = b"\x00asm";
+
+    if reader.read_bytes::<4>() != Ok(WASM_MAGIC) {
+        panic!("missing magic");
+    }
+
+    println!("Version: {:?}", reader.read_u32()?);
+    while let Ok(section_type) = reader.read::<SectionKind>() {
+        let section_size = reader.read_usize()?;
+        match section_type {
+            SectionKind::Type => {
+                let num_types = reader.read_usize()?;
+                for _ in 0..num_types {
+                    let kind = reader.read::<TypeKind>()?;
+                    match kind {
+                        TypeKind::Func => {
+                            println!("Signature: {:?}", reader.read::<FuncSignature>()?);
+                        }
+                        other => unimplemented!("{:?}", other),
+                    }
+                }
+            }
+            SectionKind::Function => {
+                let num_funcs = reader.read_usize()?;
+                for _ in 0..num_funcs {
+                    let _sig_index = reader.read_usize()?;
+                }
+            }
+            SectionKind::Export => {
+                let num_exports = reader.read_usize()?;
+                for _ in 0..num_exports {
+                    let name_len = reader.read_usize()?;
+                    let name = reader.read_slice(name_len as _)?;
+                    let name = std::str::from_utf8(name).expect("valid utf8"); // TODO
+                    let export_kind = reader.read_u8()?;
+                    let export_func_idx = reader.read_usize()?;
+                    println!("Found exported: {name} | index: {export_func_idx} | kind: {export_kind}");
+                }
+            }
+            other => unimplemented!("{:?}", other),
+        }
+        if section_size == 0 {
+            let _fixup = reader.read_usize()?;
+        }
+    }
+
+    Ok(())
 }
---
+++ src/parser.rs
@@ -0,0 +1,104 @@
+pub(crate) struct Reader<'code> {
+    data: &'code [u8],
+    pos: usize,
+}
+
+impl<'code> Reader<'code> {
+    pub(crate) fn new(data: &'code [u8]) -> Self {
+        Self {
+            data,
+            pos: 0,
+        }
+    }
+
+    pub(crate) fn read_bytes<const N: usize>(&mut self) -> Result<&'code [u8; N], ParserError> {
+        if self.pos + N < self.data.len() {
+            let bytes = self.data[self.pos..][..N].try_into()
+                .expect("enough bytes in stream");
+            self.pos += N;
+            Ok(bytes)
+        } else {
+            Err(ParserError::EndOfStream { offset: self.pos })
+        }
+    }
+
+    pub(crate) fn read_slice(&mut self, n: usize) -> Result<&'code [u8], ParserError> {
+        if self.pos + n < self.data.len() {
+            let bytes = &self.data[self.pos..][..n];
+            self.pos += n;
+            Ok(bytes)
+        } else {
+            Err(ParserError::EndOfStream { offset: self.pos })
+        }
+    }
+
+    #[inline]
+    pub(crate) fn read_u8(&mut self) -> Result<u8, ParserError> {
+        self.read_bytes::<1>().map(|b| b[0])
+    }
+
+    #[inline]
+    pub(crate) fn read_u32(&mut self) -> Result<u32, ParserError> {
+        self.read_bytes::<4>().map(|b| u32::from_le_bytes(*b))
+    }
+
+    #[inline]
+    pub(crate) fn read_usize(&mut self) -> Result<usize, ParserError> {
+        // TODO: support LEB128 encoding
+        self.read_u8().map(|b| b as usize)
+    }
+
+    #[inline]
+    pub(crate) fn read<T: Item>(&mut self) -> Result<T, ParserError> {
+        T::read(self, self.pos)
+    }
+}
+
+pub(crate) trait Item: Sized {
+    fn read(reader: &mut Reader, offset: usize) -> Result<Self, ParserError>;
+}
+
+
+#[derive(Debug)]
+#[repr(u8)]
+pub(crate) enum SectionKind {
+    Type = 0x01,
+    Function = 0x03,
+    Export = 0x07,
+    Code = 0x0A,
+}
+
+impl Item for SectionKind {
+    fn read(reader: &mut Reader, offset: usize) -> Result<Self, ParserError> {
+        match reader.read_u8()? {
+            0x01 => Ok(SectionKind::Type),
+            0x03 => Ok(SectionKind::Function),
+            0x07 => Ok(SectionKind::Export),
+            0x0A => Ok(SectionKind::Code),
+            _ => Err(ParserError::InvalidValue { offset }),
+        }
+    }
+}
+
+#[derive(Debug)]
+#[repr(u8)]
+pub(crate) enum TypeKind {
+    Func = 0x60,
+    I32 = 0x7F,
+}
+
+impl Item for TypeKind {
+    fn read(reader: &mut Reader, offset: usize) -> Result<Self, ParserError> {
+        match reader.read_u8()? {
+            0x60 => Ok(TypeKind::Func),
+            0x7F => Ok(TypeKind::I32),
+            _ => Err(ParserError::InvalidValue { offset }),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub(crate) enum ParserError {
+    EndOfStream { offset: usize },
+    InvalidValue { offset: usize },
+}
