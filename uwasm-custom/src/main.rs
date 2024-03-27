a5832b7c093ca12aaea6862a738f272623411a8f
darksv <dariusz.niedoba@gmail.com>
2024-03-27 22:27:06 +0100

    feat: add stack based vm

--- uwasm-custom/src/main.rs
+++ uwasm-custom/src/main.rs
@@ -65,8 +65,71 @@ mod reg_based {
     }
 }
 
+mod stack_based {
+    #[derive(Copy, Clone)]
+    #[repr(u8)]
+    enum Instr {
+        Push { val: u32 },
+        Add,
+        Sub,
+        JumpNonZero { target: u16 },
+        Yield,
+        Halt,
+    }
+
+    pub(crate) fn run() {
+        let code = [
+            Instr::Push { val: 123 },
+            Instr::Push { val: 1 },
+            Instr::Sub,
+            Instr::Yield,
+            Instr::JumpNonZero { target: 1 },
+            Instr::Halt,
+        ];
+
+        let mut stack = Vec::new();
+        let mut pc = 0;
+        while pc < code.len() {
+            match code[pc] {
+                Instr::Push { val } => {
+                    stack.push(val);
+                    pc += 1;
+                }
+                Instr::Add  => {
+                    let a = stack.pop().unwrap();
+                    let b = stack.pop().unwrap();
+                    stack.push(a + b);
+                    pc += 1;
+                }
+                Instr::Sub => {
+                    let a = stack.pop().unwrap();
+                    let b = stack.pop().unwrap();
+                    stack.push(b - a);
+                    pc += 1;
+                }
+                Instr::JumpNonZero { target } => {
+                    if stack.last().copied().unwrap() != 0 {
+                        pc = usize::from(target);
+                    } else {
+                        pc += 1;
+                    }
+                }
+                Instr::Yield => {
+                    println!("stack: {:?}", stack);
+                    pc += 1;
+                }
+                Instr::Halt => {
+                    pc += 1;
+                    break;
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     reg_based::run();
+    stack_based::run();
 
     println!("Hello, world!");
 }
