85a54066d1d303982932ed204cdb75d8169ff279
darksv <dariusz.niedoba@gmail.com>
2024-03-28 22:27:31 +0100

    feat(reg-vm): calling functions

--- uwasm-custom/src/main.rs
+++ uwasm-custom/src/main.rs
@@ -17,48 +17,113 @@ mod reg_based {
         JumpNonZero { src: Reg, target: u16 },
         Yield,
         Halt,
+        Call { func_idx: u16 },
+    }
+
+    struct StackFrame {
+        func_idx: usize,
+        pc: usize,
+        regs: [u32; 4],
+    }
+
+    impl StackFrame {
+        fn new(func: usize) -> Self {
+            Self {
+                func_idx: func,
+                pc: 0,
+                regs: [0u32; 4],
+            }
+        }
+    }
+
+    enum FuncBody {
+        Code(&'static [Instr]),
+        Native(u16),
+    }
+
+    struct Func {
+        code: FuncBody,
     }
 
     pub(crate) fn run() {
-        let code = [
-            Instr::SetReg { dst: Reg::R0, val: 123 },
-            Instr::SetReg { dst: Reg::R1, val: 1 },
-            Instr::Sub { dst: Reg::R0, src_a: Reg::R0, src_b: Reg::R1 },
-            Instr::Yield,
-            Instr::JumpNonZero { src: Reg::R0, target: 2 },
-            Instr::Halt,
+        let functions = [
+            Func {
+                code: FuncBody::Code(&[
+                    Instr::SetReg { dst: Reg::R0, val: 123 },
+                    Instr::SetReg { dst: Reg::R1, val: 1 },
+                    Instr::Sub { dst: Reg::R0, src_a: Reg::R0, src_b: Reg::R1 },
+                    Instr::Yield,
+                    Instr::JumpNonZero { src: Reg::R0, target: 2 },
+                    Instr::SetReg { dst: Reg::R0, val: 10 },
+                    Instr::Yield,
+                    Instr::Call { func_idx: 1 },
+                    Instr::Yield,
+                    Instr::Halt,
+                ])
+            },
+            Func {
+                code: FuncBody::Native(123),
+            }
         ];
 
-        let mut regs = [0u32; 4];
-        let mut pc = 0;
-        while pc < code.len() {
-            match code[pc] {
+        let mut call_stack: Vec<StackFrame> = Vec::new();
+        call_stack.push(StackFrame::new(0));
+
+        loop {
+            let mut frame = call_stack.last_mut().unwrap();
+            let code = match functions[frame.func_idx].code {
+                FuncBody::Code(code) => code,
+                FuncBody::Native(_) => unreachable!(),
+            };
+
+            let Some(op) = code.get(frame.pc).copied() else {
+                call_stack.pop();
+                continue;
+            };
+
+            match op {
                 Instr::SetReg { dst, val } => {
-                    regs[dst as usize] = val;
-                    pc += 1;
+                    frame.regs[dst as usize] = val;
+                    frame.pc += 1;
                 }
                 Instr::Add { src_a, src_b, dst } => {
-                    regs[dst as usize] = regs[src_a as usize].checked_add(regs[src_b as usize]).unwrap();
-                    pc += 1;
+                    frame.regs[dst as usize] = frame.regs[src_a as usize].checked_add(frame.regs[src_b as usize]).unwrap();
+                    frame.pc += 1;
                 }
                 Instr::Sub { src_a, src_b, dst } => {
-                    regs[dst as usize] = regs[src_a as usize].checked_sub(regs[src_b as usize]).unwrap();
-                    pc += 1;
+                    frame.regs[dst as usize] = frame.regs[src_a as usize].checked_sub(frame.regs[src_b as usize]).unwrap();
+                    frame.pc += 1;
                 }
                 Instr::JumpNonZero { src, target } => {
-                    if regs[src as usize] != 0 {
-                        pc = usize::from(target);
+                    if frame.regs[src as usize] != 0 {
+                        frame.pc = usize::from(target);
                     } else {
-                        pc += 1;
+                        frame.pc += 1;
                     }
                 }
                 Instr::Yield => {
-                    println!("regs: {:?}", regs);
-                    pc += 1;
+                    println!("regs: {:?}", frame.regs);
+                    frame.pc += 1;
                 }
                 Instr::Halt => {
-                    pc += 1;
+                    frame.pc += 1;
                     break;
+                }
+                Instr::Call { func_idx } => {
+                    frame.pc += 1;
+                    match functions[usize::from(func_idx)].code {
+                        FuncBody::Code(_) => {
+                            call_stack.push(StackFrame::new(usize::from(func_idx)))
+                        }
+                        FuncBody::Native(idx) => {
+                            match idx {
+                                123 => {
+                                    frame.regs[0] = frame.regs[0].pow(2);
+                                }
+                                _ => unimplemented!("calling native #{idx}"),
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -95,7 +160,7 @@ mod stack_based {
                     stack.push(val);
                     pc += 1;
                 }
-                Instr::Add  => {
+                Instr::Add => {
                     let a = stack.pop().unwrap();
                     let b = stack.pop().unwrap();
                     stack.push(a + b);
@@ -129,7 +194,6 @@ mod stack_based {
 
 fn main() {
     reg_based::run();
-    stack_based::run();
-
-    println!("Hello, world!");
+    // stack_based::run();
+    // println!("Hello, world!");
 }
